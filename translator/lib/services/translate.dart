02c7f9442869deca6e81774ac22841c56e5ab60f
yyavv <y.selim.celik.2003@hotmail.com>
2024-09-12 01:58:31 +0300

    changes

--- translator/lib/services/translate.dart
+++ translator/lib/services/translate.dart
@@ -1,4 +1,5 @@
 import 'package:google_generative_ai/google_generative_ai.dart';
+import "package:flutter_dotenv/flutter_dotenv.dart";
 
 class Translator {
   Translator({required this.text, required this.start, required this.dest});
@@ -8,7 +9,8 @@ class Translator {
   final String _apiKey = "AIzaSyDQH2DQ-2DtSnDpKp9n8rgIzbAfEZoJ6OU";
 
   Future<String> generateText() async {
-    final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: _apiKey);
+    final model = GenerativeModel(
+        model: 'gemini-1.5-flash', apiKey: dotenv.env['API_KEY'] ?? '');
     final content = [
       Content.text(
           'Please translate the following text into $dest. Provide only the translated text without any additional explanations, introductions, or comments: $text')
